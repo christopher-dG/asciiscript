8abf269b1212a35404d90974db1528932c833956
Chris de Graaf <chrisadegraaf@gmail.com>
2018-10-22 22:09:48 -0500

    Record asciicast
    Everything works except for the 'Press enter to upload or ctrl-c
    to save locally' prompt at the end. I'm pretty sure it's due to
    the carriage return printed by asciinema.

--- main.go
+++ main.go
@@ -3,23 +3,24 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
+	"os/signal"
 	"strconv"
 	"strings"
 	"time"
 )
 
-const ControlPrefix = "#$"
+const CtrlPrefix = "#$"
 
 var (
-	Usage         = "usage: " + os.Args[0] + " <script>"
-	ErrUnknownCmd = errors.New("unknown command")
-	ErrNoArgs     = errors.New("no arguments given to command")
-	ErrBadArg     = errors.New("invalid command argument")
+	Usage          = "usage: " + os.Args[0] + " <script>"
+	ErrUnknownCtrl = errors.New("unknown control command")
+	ErrNoArgs      = errors.New("no arguments given to command")
+	ErrBadArg      = errors.New("invalid command argument")
 )
 
 func main() {
@@ -27,22 +28,29 @@ func main() {
 		log.Fatal(Usage)
 	}
 	if exec.Command("asciinema", "-h").Run() != nil {
-		log.Fatal("asciinema is not installed")
+		log.Fatal("can't find asciinema executable")
 	}
 
-	s, err := NewScript(os.Args[1])
+	s, err := NewScript(os.Args[1], os.Args[2:])
 	if err != nil {
-		log.Fatal("parsing script failed:", err)
+		log.Fatal("parsing script failed: ", err)
 	}
 
-	if err := s.Run(); err != nil {
-		log.Fatal(err)
+	if err := s.Start(); err != nil {
+		log.Fatal("couldn't start recording: ", err)
 	}
+	defer func() {
+		if err := s.Stop(); err != nil {
+			log.Fatal("couldn't stop recording: ", err)
+		}
+	}()
+
+	s.Execute()
 }
 
 // Command is an action to be run.
 type Command interface {
-	Run(*Script) error
+	Run(*Script)
 }
 
 // Shell is a shell command to execute.
@@ -52,19 +60,23 @@ type Shell struct {
 
 // NewShell creates a new Shell.
 func NewShell(cmd string) Shell {
+	if !strings.HasSuffix(cmd, "\n") {
+		cmd += "\n"
+	}
 	return Shell{Cmd: cmd}
 }
 
 // Run runs the shell command.
-func (s Shell) Run(sc *Script) error {
+func (s Shell) Run(sc *Script) {
 	for _, c := range s.Cmd {
-		fmt.Printf("%s", string(c))
-		time.Sleep(sc.TypingDelay)
+		if _, err := sc.Stdin.Write([]byte(fmt.Sprintf("%s", string(c)))); err != nil {
+			os.Exit(1)
+		}
+		time.Sleep(sc.Delay)
 	}
-	return nil
 }
 
-// Wait is a command to wait for some time.
+// Wait is a command to change the interval between commands.
 type Wait struct {
 	Duration time.Duration
 }
@@ -75,7 +87,7 @@ func NewWait(opts []string) (Wait, error) {
 		return Wait{}, ErrNoArgs
 	}
 
-	ms, err := strconv.ParseInt(opts[0], 10, 64)
+	ms, err := strconv.ParseInt(strings.TrimSpace(opts[0]), 10, 64)
 	if err != nil {
 		return Wait{}, ErrBadArg
 	}
@@ -83,13 +95,12 @@ func NewWait(opts []string) (Wait, error) {
 	return Wait{Duration: time.Millisecond * time.Duration(ms)}, nil
 }
 
-// Run waits for the specified amount of time.
-func (w Wait) Run(*Script) error {
-	time.Sleep(w.Duration)
-	return nil
+// Run changes the wait for subsequent commands.
+func (w Wait) Run(s *Script) {
+	s.Wait = w.Duration
 }
 
-// Delay is a command to change the typing speed of other commands.
+// Delay is a command to change the typing speed of subsequent commands.
 type Delay struct {
 	Interval time.Duration
 }
@@ -100,7 +111,7 @@ func NewDelay(opts []string) (Delay, error) {
 		return Delay{}, ErrNoArgs
 	}
 
-	ms, err := strconv.ParseInt(opts[0], 10, 64)
+	ms, err := strconv.ParseInt(strings.TrimSpace(opts[0]), 10, 64)
 	if err != nil {
 		return Delay{}, ErrBadArg
 	}
@@ -108,75 +119,142 @@ func NewDelay(opts []string) (Delay, error) {
 	return Delay{Interval: time.Millisecond * time.Duration(ms)}, nil
 }
 
-// Run changes the speed for subsequent commands.
-func (s Delay) Run(sc *Script) error {
-	sc.TypingDelay = s.Interval
-	return nil
+// Run changes the typing speed for subsequent commands.
+func (s Delay) Run(sc *Script) {
+	sc.Delay = s.Interval
 }
 
-// NewControlCommand creates a new control command.
-func NewControlCommand(cmd string) (Command, error) {
+// NewCtrl creates a new control command.
+func NewCtrl(cmd string) (Command, error) {
 	tokens := strings.Split(cmd, ":")
-	switch tokens[0] {
+	switch strings.TrimSpace(tokens[0]) {
 	case "delay":
 		return NewDelay(tokens[1:])
 	case "wait":
 		return NewWait(tokens[1:])
 	default:
-		return nil, ErrUnknownCmd
+		return nil, ErrUnknownCtrl
 	}
 }
 
 // Script is a shell script to be run and recorded by asciinema.
 type Script struct {
-	Name        string
-	Commands    []Command
-	TypingDelay time.Duration
+	Args     []string
+	Commands []Command
+	Delay    time.Duration
+	Wait     time.Duration
+	Cmd      *exec.Cmd
+	Stdin    io.WriteCloser
+	Stdout   io.ReadCloser
+	Stderr   io.ReadCloser
 }
 
-// NewScript parses a new Script from the file at path.
-func NewScript(path string) (*Script, error) {
+// NewScript parses a new Script from the script file at path.
+func NewScript(path string, args []string) (*Script, error) {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	s := &Script{TypingDelay: time.Millisecond * 25}
-	s.Name, _ = filepath.Abs(path)
+	s := &Script{
+		Args:  args,
+		Delay: time.Millisecond * 20,
+		Wait:  time.Millisecond * 100,
+	}
 
 	lines := strings.Split(string(b), "\n")
-	curCmd := ""
 	for i, line := range lines {
 		if line == "" {
 			continue
 		}
-		if strings.HasPrefix(line, ControlPrefix) {
-			if curCmd != "" {
-				s.Commands = append(s.Commands, NewShell(curCmd))
-				curCmd = ""
-			}
-			ctrl, err := NewControlCommand(line[2:])
+		if strings.HasPrefix(line, CtrlPrefix) {
+			ctrl, err := NewCtrl(line[len(CtrlPrefix):])
 			if err != nil {
-				return nil, fmt.Errorf("%v (line %d)", err, i)
+				return nil, fmt.Errorf("%v (line %d)", err, i+1)
 			}
 			s.Commands = append(s.Commands, ctrl)
 		} else {
-			curCmd += line + "\n"
+			s.Commands = append(s.Commands, NewShell(line))
 		}
-	}
-	if curCmd != "" {
-		s.Commands = append(s.Commands, NewShell(curCmd[:len(curCmd)-1]))
 	}
 
 	return s, nil
 }
 
-// Run runs the script.
-func (s *Script) Run() error {
-	for i, c := range s.Commands {
-		if err := c.Run(s); err != nil {
-			return fmt.Errorf("%v (command %d)", err, i)
-		}
+// Start starts recording.
+func (s *Script) Start() error {
+	args := append([]string{"rec"}, s.Args...)
+	s.Cmd = exec.Command("asciinema", args...)
+	var err error
+	if s.Stdin, err = s.Cmd.StdinPipe(); err != nil {
+		return err
+	}
+	if s.Stdout, err = s.Cmd.StdoutPipe(); err != nil {
+		return err
+	}
+	if s.Stderr, err = s.Cmd.StderrPipe(); err != nil {
+		return err
+	}
+	if err = s.Cmd.Start(); err != nil {
+		return err
+	}
+	go echo(s.Stdout)
+	go echo(s.Stderr)
+	return nil
+}
+
+// Stop stops recording.
+func (s *Script) Stop() error {
+	defer s.Cmd.Wait()
+	if _, err := s.Stdin.Write([]byte("exit\n")); err != nil {
+		return err
+	}
+	if len(s.Args) == 0 || strings.HasPrefix(s.Args[0], "-") {
+		s.endDialog()
 	}
 	return nil
+}
+
+func (s *Script) endDialog() {
+	handler := make(chan os.Signal, 1)
+	sig := make(chan os.Signal, 1)
+	stdin := make(chan bool, 1)
+	signal.Notify(handler, os.Interrupt)
+
+	go func() {
+		sig <- <-handler
+		signal.Stop(handler)
+
+	}()
+	go func() {
+		fmt.Scanln()
+		stdin <- true
+	}()
+
+	select {
+	case int := <-sig:
+		s.Cmd.Process.Signal(int)
+	case <-stdin:
+		s.Stdin.Write([]byte{'\n'})
+	}
+}
+
+// Execute runs the script's commands.
+func (s *Script) Execute() {
+	for _, c := range s.Commands {
+		c.Run(s)
+		time.Sleep(s.Wait)
+	}
+}
+
+// echo prints out output continously.
+func echo(r io.Reader) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf)
+		if err != nil {
+			return
+		}
+		fmt.Print(string(buf[:n]))
+	}
 }
